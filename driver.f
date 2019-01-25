c62ce1eeeb0a7848dbe9f4575811f9cc4e542dde
Bjørn Håkon Frodal <bjorn.h.frodal@ntnu.no>
2019-01-25 16:34:34 +0100

    Removed GOTO

--- driver.f
+++ driver.f
@@ -10,7 +10,8 @@
       implicit none
       real*8, allocatable :: ang(:,:), STRESSOLD(:,:), STRESSNEW(:,:),
      .                  STATEOLD(:,:), STATENEW(:,:), defgradNew(:,:),
-     .                  defgradOld(:,:), Dissipation(:), Dij(:,:)
+     .                  defgradOld(:,:), Dissipation(:), Dij(:,:),
+     .                  sigma(:,:)
 	  integer nDmax,nprops
       integer k,ITER,ndef,i,km,planestress,centro,npts
       integer NITER,NSTATEV,nblock
@@ -20,13 +21,12 @@
 	  real*8 domega32,domega13,domega21
       CHARACTER*12 DATE1,TIME1
 c
-      real*8 TIME,printDelay,currentTime,printTime
+      real*8 printDelay,currentTime,printTime
       real*8 DT
       real*8 PROPS(nprops)
 	  real*8 work
 	  real*8 totweight
       real*8 oneovertotweight
-	  real*8 sigma(6)
       real*8 one,zero
       parameter(zero=0.d0,one=1.d0,printDelay=5.d0*60.d0)
 !-----------------------------------------------------------------------
@@ -63,15 +63,12 @@ c
 	  endif
       allocate(Dij(nDmax,6))
       call deformation(Dij,nDmax,ndef,planestress,centro,npts,epsdot)
+      allocate(sigma(ndef,7))
+      sigma = zero
 c
-      NITER  = 100001 ! Max number of iterations
+      NITER  = 10001 ! Max number of iterations
 c
       DT     = wp/(epsdot*props(6)*1.d3) ! dt=wp/(M*Niter*tauc_0*epsdot) (M=3,Niter=1000)
-!-----------------------------------------------------------------------
-!     Write to file
-!-----------------------------------------------------------------------
-	  open (unit = 2, file = ".\Output\output.txt")
-      WRITE(2,*) 'S11, S22, S33, S12, S23, S31, wp'
 !-----------------------------------------------------------------------
 !     Write to start date and time
 !-----------------------------------------------------------------------
@@ -85,74 +82,47 @@ c
 !-----------------------------------------------------------------------
 !     Loop over deformation points
 !-----------------------------------------------------------------------
-	  km = 0
-   12 continue
-	  km=km+1
-	  if (km.gt.ndef) then
-			goto 51
-	  endif
+      do km=1,ndef
 !-----------------------------------------------------------------------
 !     Initialize some variables
 !-----------------------------------------------------------------------
-      TIME = zero
 	  work = zero
 c
-      do i=1,nblock
-        STRESSOLD(i,1) = zero
-        STRESSNEW(i,1) = zero
-        STRESSOLD(i,2) = zero
-        STRESSNEW(i,2) = zero
-        STRESSOLD(i,3) = zero
-        STRESSNEW(i,3) = zero
-        STRESSOLD(i,4) = zero
-        STRESSNEW(i,4) = zero
-        STRESSOLD(i,5) = zero
-        STRESSNEW(i,5) = zero
-        STRESSOLD(i,6) = zero
-        STRESSNEW(i,6) = zero
-	  enddo
+      STRESSOLD = zero
+      STRESSNEW = zero
 c
-      do k=1,NSTATEV
+      do i=1,nblock
+		STATEOLD(i,1) = ang(i,1)
+		STATEOLD(i,2) = ang(i,2)
+		STATEOLD(i,3) = ang(i,3)
+	  enddo
+      do k=4,NSTATEV
 		do i=1,nblock
 			STATEOLD(i,k) = zero
 			STATENEW(i,k) = zero
 		enddo
 	  enddo
-	  do i=1,nblock
-		STATEOLD(i,1) = ang(i,1)
-		STATEOLD(i,2) = ang(i,2)
-		STATEOLD(i,3) = ang(i,3)
-	  enddo
 c
 	  do i=1,nblock
         defgradOld(i,1) = one
-        defgradNew(i,1) = one
         defgradOld(i,2) = one
-        defgradNew(i,2) = one
         defgradOld(i,3) = one
-        defgradNew(i,3) = one
         defgradOld(i,4) = zero
-        defgradNew(i,4) = zero
         defgradOld(i,5) = zero
-        defgradNew(i,5) = zero
         defgradOld(i,6) = zero
-        defgradNew(i,6) = zero
         defgradOld(i,7) = zero
-        defgradNew(i,7) = zero
         defgradOld(i,8) = zero
-        defgradNew(i,8) = zero
         defgradOld(i,9) = zero
-        defgradNew(i,9) = zero
 	  enddo
 !-----------------------------------------------------------------------
 !     Start loop
 !-----------------------------------------------------------------------
-      do ITER=1,NITER
+      iter = 0
+      do while((work.lt.wp).and.(ITER.lt.NITER))
+        iter = iter+1
 !-----------------------------------------------------------------------
 !        Extract strain increments 
 !-----------------------------------------------------------------------
-!         if(time.gt.zero)then
-c
             do i=1,nblock
 				defgradNew(i,1) = defgradOld(i,1)*(Dij(km,1)*dt+one)
      +           +defgradOld(i,7)*Dij(km,4)*dt
@@ -182,7 +152,6 @@ c
      +           +defgradOld(i,5)*Dij(km,4)*dt
      +           +defgradOld(i,3)*Dij(km,6)*dt
 			enddo
-!         endif
 !-----------------------------------------------------------------------
 !        CALL UMAT
 !-----------------------------------------------------------------------
@@ -190,76 +159,62 @@ c
      +               stressOld,stateOld,defgradOld,dt,props,
      +               nblock,3,3,nstatev,nprops,Dissipation)
 !-----------------------------------------------------------------------
-!        UPDATE TIME
-!-----------------------------------------------------------------------
-         TIME = TIME+DT
-!-----------------------------------------------------------------------
 !        UPDATE VARIABLES FOR NEXT TIME STEP
 !-----------------------------------------------------------------------
-         do i=1,nblock
-			STRESSOLD(i,1) = STRESSNEW(i,1)
-            STRESSOLD(i,2) = STRESSNEW(i,2)
-            STRESSOLD(i,3) = STRESSNEW(i,3)
-            STRESSOLD(i,4) = STRESSNEW(i,4)
-            STRESSOLD(i,5) = STRESSNEW(i,5)
-            STRESSOLD(i,6) = STRESSNEW(i,6)
-		 enddo
-		 do k=1,NSTATEV
-			do i=1,nblock
-				STATEOLD(i,k) = STATENEW(i,k)
-			enddo
-		 enddo
-		 do i=1,nblock
-			defgradOld(i,1) = defgradNew(i,1)
-            defgradOld(i,2) = defgradNew(i,2)
-            defgradOld(i,3) = defgradNew(i,3)
-            defgradOld(i,4) = defgradNew(i,4)
-            defgradOld(i,5) = defgradNew(i,5)
-            defgradOld(i,6) = defgradNew(i,6)
-            defgradOld(i,7) = defgradNew(i,7)
-            defgradOld(i,8) = defgradNew(i,8)
-            defgradOld(i,9) = defgradNew(i,9)
-		 enddo
+         STRESSOLD = STRESSNEW
+         STATEOLD = STATENEW
+         defgradOld = defgradNew
 		 do i=1,nblock
 			work = work + Dissipation(i)*ang(i,4)*oneovertotweight
 		 enddo
-		 if (work.ge.wp) then
-            call cpu_time(currentTime)
-            if((currentTime.ge.(printTime+printDelay)).or.
-     .         (km.eq.ndef))then
-               printTime = printTime+printDelay
-               write(6,*) 'Deformation points completed: ',
-     .                    km, ' of ', ndef
-            endif
-!            write(6,*) iter
-            sigma = zero
-            do i=1,nblock
-               sigma(1) = sigma(1)+STRESSNEW(i,1)*ang(i,4)
-			   sigma(2) = sigma(2)+STRESSNEW(i,2)*ang(i,4)
-			   sigma(3) = sigma(3)+STRESSNEW(i,3)*ang(i,4)
-			   sigma(4) = sigma(4)+STRESSNEW(i,4)*ang(i,4)
-			   sigma(5) = sigma(5)+STRESSNEW(i,5)*ang(i,4)
-			   sigma(6) = sigma(6)+STRESSNEW(i,6)*ang(i,4)
-            enddo
-            sigma = sigma*oneovertotweight
-			sigma(1) = sigma(1)-sigma(3)	! Superpose a hydrostatic stress so that s33=0
-			sigma(2) = sigma(2)-sigma(3)	! Yielding is not dependent upon hydrostatic stress!
-			sigma(3) = sigma(3)-sigma(3)
-			write(2,98) sigma(1),sigma(2),sigma(3),sigma(4),
-     +                  sigma(5),sigma(6), work
-			goto 12
-		 endif
-		 if (ITER.ge.NITER) then
-			write(6,*) '!! Error'
-			write(6,*) 'Maximum number of iterations reached'
-            close(2)
-			stop
-		 endif
       enddo
-   51 continue
+      if (work.ge.wp) then
+!-----------------------------------------------------------------------
+!     Write to window if enough time has passed
+!-----------------------------------------------------------------------
+        call cpu_time(currentTime)
+        if((currentTime.ge.(printTime+printDelay)).or.
+     .      (km.eq.ndef))then
+            printTime = printTime+printDelay
+            write(6,*) 'Deformation points completed: ',
+     .                  km, ' of ', ndef
+        endif
+!        write(6,*) iter
+!-----------------------------------------------------------------------
+!     Calculate stress based on the Taylor hypothesis
+!-----------------------------------------------------------------------
+        do i=1,nblock
+            sigma(km,1) = sigma(km,1)+STRESSNEW(i,1)*ang(i,4)
+            sigma(km,2) = sigma(km,2)+STRESSNEW(i,2)*ang(i,4)
+            sigma(km,3) = sigma(km,3)+STRESSNEW(i,3)*ang(i,4)
+            sigma(km,4) = sigma(km,4)+STRESSNEW(i,4)*ang(i,4)
+            sigma(km,5) = sigma(km,5)+STRESSNEW(i,5)*ang(i,4)
+            sigma(km,6) = sigma(km,6)+STRESSNEW(i,6)*ang(i,4)
+        enddo
+        sigma(km,1) = sigma(km,1)-sigma(km,3)	! Superpose a hydrostatic stress so that s33=0
+        sigma(km,2) = sigma(km,2)-sigma(km,3)	! Yielding is not dependent upon hydrostatic stress!
+        sigma(km,3) = sigma(km,3)-sigma(km,3)
+        sigma(km,7) = work
+        sigma(km,1:6) = sigma(km,1:6)*oneovertotweight
+      endif
+      if (ITER.ge.NITER) then
+        write(6,*) '!! Error'
+        write(6,*) 'Maximum number of iterations reached'
+        stop
+      endif
+      enddo
+!-----------------------------------------------------------------------
+!     Write the result to file
+!-----------------------------------------------------------------------
+      open (unit = 2, file = ".\Output\output.txt")
+      WRITE(2,*) 'S11, S22, S33, S12, S23, S31, wp'
+      do km=1,ndef
+        write(2,98) sigma(km,1),sigma(km,2),sigma(km,3),sigma(km,4),
+     +              sigma(km,5),sigma(km,6), sigma(km,7)
+      enddo
 	  close(2)
 !-----------------------------------------------------------------------
-!     Write to start date and time
+!     Write to finish date and time
 !-----------------------------------------------------------------------
       write(6,*) '----------------------------------------------------'
       CALL DATE_AND_TIME(DATE1,TIME1)
