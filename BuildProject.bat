42ef9fcfdc57c3de722cf5b7d083abdc32488abf
Bjørn Håkon Frodal <bfrodal@gmail.com>
2019-10-23 14:29:33 +0200

    Added build script

---
+++ BuildProject.bat
@@ -0,0 +1,14 @@
+@echo off
+call ifort -openmp -F1000000000 main.f -o ./GUI/Core/FC-Taylor.exe -O3
+if %ERRORLEVEL% neq 0 (
+  echo.
+  echo Could not compile the Fortran source!
+  echo The program was therefore not built!
+  echo.
+  pause
+  exit
+)
+pushd %~dp0\GUI\
+call npm run build-win32
+call npm run setup
+popd
