436051b762ab9d50e5a39cce9889d7d5b41d48c9
Bjørn Håkon Frodal <bfrodal@gmail.com>
2020-03-31 14:34:28 +0200

    Using FC-Taylor.json to check for new version

--- GUI/src/javascript/license.js
+++ GUI/src/javascript/license.js
@@ -1,20 +1,18 @@
 ////////////////////////////////////////////////////////////////////////////////////
 //                                 Lisence check                                  //
 ////////////////////////////////////////////////////////////////////////////////////
-const {app, dialog, BrowserWindow, ipcMain} = require('electron');
+const { app, dialog, BrowserWindow, ipcMain } = require('electron');
 const bent = require('bent')
 const getJSON = bent('json')
 
 const LicenseLocation = 'http://folk.ntnu.no/frodal/Cite/Projects/FC-Taylor.json';
 let LicenseOK = false;
 
-async function CheckLicense()
-{
+async function CheckLicense() {
     getJSON(LicenseLocation)
         .then(value => {
             ValidateLicense(value);
-            if (!LicenseOK) 
-            {
+            if (!LicenseOK) {
                 dialog.showErrorBox('Error', 'The version of the program you are using is deprecated.\nPlease request a new version from the distributer.\nContact: bjorn.h.frodal@ntnu.no');
                 app.quit();
             }
@@ -28,36 +26,29 @@ async function CheckLicense()
                     buttons: ['Try again', 'Quit'],
                     cancelId: 1
                 });
-            if (choice === 0) 
-            {
+            if (choice === 0) {
                 CheckLicense();
-            } else 
-            {
+            } else {
                 app.quit();
             }
         });
 }
 
-function ValidateLicense(value) 
-{
+function ValidateLicense(value) {
     let newestVersion = value.version.split('.')
     let currentVersion = app.getVersion().split('.');
-    if (newestVersion.length !== currentVersion.length) 
-    {
+    if (newestVersion.length !== currentVersion.length) {
         SendToRenderer(false);
         return
     }
-    for (let i = 0; i < currentVersion.length; ++i) 
-    {
+    for (let i = 0; i < currentVersion.length; ++i) {
         newestVersion[i] = parseFloat(newestVersion[i])
         currentVersion[i] = parseFloat(currentVersion[i])
-        if (newestVersion[i] > currentVersion[i]) 
-        {
+        if (newestVersion[i] > currentVersion[i]) {
             SendToRenderer(false);
             return
         }
-        else if (newestVersion[i] < currentVersion[i]) 
-        {
+        else if (newestVersion[i] < currentVersion[i]) {
             SendToRenderer(true);
             return
         }
@@ -65,17 +56,77 @@ function ValidateLicense(value)
     SendToRenderer(true);
 }
 
-function SendToRenderer(value)
-{
+function SendToRenderer(value) {
     LicenseOK = value;
     let win = BrowserWindow.getFocusedWindow();
-    if(win)
-    {
-        win.send('LicenseCheck',value);
+    if (win) {
+        win.send('LicenseCheck', value);
     }
 }
 
-ipcMain.on('CheckLicensePlease',CheckLicense);
+async function CheckVersion() {
+    getJSON(LicenseLocation)
+        .then(value => {
+            let newestVersion = value.newestVersion.split('.')
+            let currentVersion = app.getVersion().split('.');
+            if (newestVersion.length !== currentVersion.length) {
+                openVersionDialog(false);
+                return
+            }
+            for (let i = 0; i < currentVersion.length; ++i) {
+                newestVersion[i] = parseFloat(newestVersion[i])
+                currentVersion[i] = parseFloat(currentVersion[i])
+                if (newestVersion[i] > currentVersion[i]) {
+                    openVersionDialog(false);
+                    return
+                }
+                else if (newestVersion[i] < currentVersion[i]) {
+                    openVersionDialog(true);
+                    return
+                }
+            }
+            openVersionDialog(true);
+        }).catch(error => {
+            const options =
+            {
+                type: "info",
+                title: "Connection error",
+                message: "Could not connect to the license server.\nPlease check your internet connection and try again.",
+                buttons: ['Ok']
+            };
+            dialog.showMessageBox(BrowserWindow.getFocusedWindow(), options);
+        });
+}
+
+function openVersionDialog(uptoDate) {
+    if (uptoDate) {
+        const options =
+        {
+            type: "info",
+            title: "You're all good",
+            message: "You've got the latest version of " + app.name + "; thanks for staying on the ball",
+            buttons: ['Ok']
+        };
+        dialog.showMessageBox(BrowserWindow.getFocusedWindow(), options);
+    } else {
+        const options =
+        {
+            type: "info",
+            title: "New version available",
+            message: "A new version of " + app.name + " is available.\nContact: bjorn.h.frodal@ntnu.no",
+            buttons: ['Ok']
+        };
+        dialog.showMessageBox(BrowserWindow.getFocusedWindow(), options);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+
+// Check license request from the renderer process
+ipcMain.on('CheckLicensePlease', CheckLicense);
 
 // Repetatly check license every 10 min
-setInterval(CheckLicense,600000);
+setInterval(CheckLicense, 600000);
+
+// Exports
+exports.CheckVersion = CheckVersion;
