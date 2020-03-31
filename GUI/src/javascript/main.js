7d07d3defdded08557fb873365774c9bd34fbaf0
Bjørn Håkon Frodal <bfrodal@gmail.com>
2020-03-31 12:57:56 +0200

    Moved license check to main process

--- GUI/src/javascript/main.js
+++ GUI/src/javascript/main.js
@@ -87,6 +87,7 @@ app.on('activate', function ()
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 require('./dialog');
+require('./license');
 
 // Sets the working directory path variable, i.e., the temp folder to delete on quit
 ipcMain.on('core-temp', (event,arg)=>
