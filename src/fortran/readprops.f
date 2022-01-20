40849b89e672d1079c46c55dee5959973ead98b5
Bjørn Håkon Frodal <bfrodal@gmail.com>
2022-01-20 11:11:28 +0100

    v1.0.0

--- src/fortran/readprops.f
+++ src/fortran/readprops.f
@@ -140,7 +140,7 @@
       write(6,*) '|  Norwegian University of Science and Technology, |'
       write(6,*) '|                Trondheim, Norway.                |'
       write(6,*) '|                                                  |'
-      write(6,*) '|                      v0.15.0                     |'
+      write(6,*) '|                      v1.0.0                      |'
       write(6,*) '|                                                  |'
       write(6,*) '|--------------------------------------------------|'
       write(6,*) '|                                                  |'
